63983b020a3dc4607ef080645fe7b5fc075f97b0
Ihr Name <doersam@hdm-stuttgart.de>
2019-11-15 08:59:52 +0100

    2 Varianten

--- V04_Koordinaten/src/loesung/Punkt.js
+++ V04_Koordinaten/src/loesung/Punkt.js
@@ -1,39 +1,49 @@
 class Punkt{
 
+    //Konstruktor
     constructor(){
-        this.x=0;
-        this.y=0;
+        this.x = 0;
+        this.y = 0;
     }
 
+    /*
+     constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+    */
+
+    //Quadrant Methode
     quadrant(){
         let q;
-        if(this.x>0 && this.y>0){
-            q=1;
+        if(this.x > 0 && this.y > 0){
+            q = 1;
             return q;
         }
-        else if(this.x<0&&this.y>0){
-            q=2;
+        else if(this.x < 0 && this.y > 0){
+            q = 2;
             return q;
         }
-        else if(this.x<0&&this.y<0){
-            q=3;
+        else if(this.x < 0 && this.y < 0){
+            q = 3;
             return q;
         }
-        else if(this.x>0&&this.y<0){
-            q=4;
+        else if(this.x > 0 && this.y < 0){
+            q = 4;
             return q;
         }
         else {
-            q="Ursprung";
+            q = 0;
             return q;
         }
     }
 
+    // Entfernung vom Ursprung Methode
     entfernungVomUrsprung(){
-        let a= this.x;
-        let b=this.y;
+        let a = this.x;
+        let b = this.y;
         let c;
-        let py=(a*a)+(b*b);
+        let py = (a*a) + (b*b);
         c= Math.sqrt(py);
         return c;
     }
--- V04_Koordinaten/src/loesung/punkteTest.js
+++ V04_Koordinaten/src/loesung/punkteTest.js
@@ -1,3 +1,4 @@
+//Neue Punkte belegen
 let p1= new Punkt();
 p1.x=24;
 p1.y=18;
@@ -14,8 +15,16 @@ let p4= new Punkt();
 p4.x=-16;
 p4.y=12;
 
+/*
+let p1= new Punkt(24,18);
+let p2= new Punkt(-3,-4);
+let p3= new Punkt(8,6);
+let p4= new Punkt(-16,12);
+
+*/ 
 
 
+//Funktionsdefinition
 function ausgabe(){
     let parr=[p1,p2,p3,p4]
     for (let i=0; i<parr.length; i++){
@@ -26,4 +35,5 @@ console.log("Der Punkt mit den Koordinaten "+parr[i].x+
 " und "+parr[i].y+" liegt im "+qua+
 ".ten Quadranten. Seine Entfernung beträgt: "+ent);}}
 
+//Funktionsaufruf
 ausgabe();
