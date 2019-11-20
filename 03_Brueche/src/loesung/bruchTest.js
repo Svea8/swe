b9382d1e28002bc4a5a5da0bb23c0b439219a3d4
Ihr Name <doersam@hdm-stuttgart.de>
2019-11-20 17:03:40 +0100

    Bruch letztes

--- 03_Brueche/src/loesung/bruchTest.js
+++ 03_Brueche/src/loesung/bruchTest.js
@@ -1,7 +1,7 @@
 let bruch1= new Bruch(1,2);
 let bruch2= new Bruch(4,2);
 
-let erwe= bruch1.erweitern(6);
+//let erwe= bruch1.erweitern(6);
 //let kür= bruch1.kuerzen(2);
 
 let add= bruch1.addieren(bruch2);
@@ -10,7 +10,7 @@ let mul= bruch2.multiplizieren(bruch2);
 console.log("Ausgabe: "+bruch1.ausgabe());
 console.log("Ausgabe addieren: "+add.ausgabe());
 console.log("Ausgabe multiplizieren: "+mul.ausgabe());
-console.log("Erweitern: "+erwe.ausgabe());
+//console.log("Erweitern: "+erwe.ausgabe());
 
 
 let b=new BruchHelfer();
