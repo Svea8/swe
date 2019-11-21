8ce460d8045855539a87ffdaa468b71b9cbf9e19
Ihr Name <doersam@hdm-stuttgart.de>
2019-11-21 12:58:19 +0100

    Vorübung

---
+++ V07_Autorenliste/src/loesung/Autor.js
@@ -0,0 +1,20 @@
+class Autor{
+
+
+
+    constructor(nachname=null, alter=-1){
+        this.nachname=nachname;
+        this.geburtsjahr=2019-alter;
+    }
+
+    equals(a){
+        if (this.geburtsjahr==a.geburtsjahr && this.nachname==a.nachname){
+
+        }
+    }
+    toString(){
+        let ausgabe=;
+        return ausgabe
+    }
+
+}
