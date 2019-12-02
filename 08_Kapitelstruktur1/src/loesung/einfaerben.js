6daeb6ce5a9663ceccbc8cad8da07ac4bc673fc3
Ihr Name <doersam@hdm-stuttgart.de>
2019-12-02 13:20:11 +0100

    Hauptübung 2 Anfang 02.12.2019

--- 08_Kapitelstruktur1/src/loesung/einfaerben.js
+++ 08_Kapitelstruktur1/src/loesung/einfaerben.js
@@ -2,6 +2,6 @@ h1Dekoration();
 
 function h1Dekoration(){
     let h1 = document.getElementsByTagName('h1');
-    h1.style.fontSize="50px";
-    h1.style.textDecoration="underline";
+    h1[0].style.fontSize="50px";
+    h1[0].style.textDecoration="underline";
 }
