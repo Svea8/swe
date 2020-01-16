973b3fa3812158334d3088fc2e8037432b3e31d5
Svea <sb263@hdm-stuttgart.de>
2020-01-16 17:59:55 +0100

    16.01.

--- V15_Validierung/src/loesung/eingabe.js
+++ V15_Validierung/src/loesung/eingabe.js
@@ -11,32 +11,38 @@ function eingabeValidieren(){
     console.log(test);
     return test;
 }
-
+let span1=document.createElement('span');
 function checkName(){
     let check=true;
     let name=document.getElementsByName("name")[0];
+    
     if(name.value.length<2 || name.value.length>51){
         check=false;
         //alert("falscher name");
         name.style.backgroundColor="red";
-        let span=document.createElement('span');
-        span.innerHTML="falscher name";
-        name.appendChild(span);
+        span1.innerHTML="falscher name";
+        name.parentNode.appendChild(span1);
         return check;
     }
     name.style.backgroundColor="white";
+    span1.innerHTML="";
     return check;
 }
 
 function checkAlter(){
     let check=true;
     let alter=document.getElementsByName("alter")[0];
+    //let span=document.createElement('span');
     if(alter.value!=0 && (alter.value<=0 || alter.value>=110)){
         check=false;
         alter.style.backgroundColor="red";
+        
+        span1.innerHTML="falsches alter";
+        alter.parentNode.appendChild(span1);
         return check;
     }
     alter.style.backgroundColor="white";
+    span1.innerHTML="";
     return check;
 }
 
@@ -48,10 +54,14 @@ function checkMail(){
     if(email.value.length>4 && a<b-1 && a>-1 && b>-1){
         check=true;
         email.style.backgroundColor="white";
+        span1.innerHTML="";
     }
     else{
         check=false;
         email.style.backgroundColor="red";    
+        //let span=document.createElement('span');
+        span1.innerHTML="falsche mail";
+        email.parentNode.appendChild(span1);
     }
     return check;
 }
